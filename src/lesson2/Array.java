17b839669bec2be29829e99d9850d647e765092e
Skopin <Skopin_MI@mail.ru>
2021-02-06 09:31:31 +0300

    HomeWork

---
+++ src/lesson2/Array.java
@@ -0,0 +1,27 @@
+package lesson2;
+
+public interface Array<E> {
+
+    void add(E value);
+
+    E get(int index);
+
+    boolean remove(E value);
+
+    E remove(int index);
+
+    int indexOf(E value);
+
+    boolean contains(E value);
+
+    boolean isEmpty();
+
+    int size();
+
+    void display();
+
+    void sortBubble();
+    void sortSelect();
+    void sortInsert();
+
+}
